603f154d05d69fa10c2dcf980751a4d8b373a017
hirosansan33 <hirosan1031_0802@yahoo.co.jp>
2022-08-12 23:10:43 +0900

    作成

---
+++ WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
